6485b7fff56779e1978e3041184ad5c13e808348
hrithikkrish <80141827+hrithikkrish@users.noreply.github.com>
2021-03-20 23:18:25 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,8 +3,7 @@ a=int(input("Enter the terms"))
 f=0                                         
 s=1                                         
 if a<=0:
-    print("The requested series is
-",f)
+    print("The requested series is",f)
 else:
     print(f,s,end=" ")
     for x in range(2,a):
